bb8f313ce3b6849e57fcfae378eac5c12090981b
Kea-Meng <keameng9@gmail.com>
2023-11-23 14:02:33 +0700

    Power(x,n)

--- recursion.java
+++ recursion.java
@@ -1,15 +1,23 @@
 public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    public static void main(String[] args) {
+        // Example usage
+        double result = power(2, 3);
+        System.out.println("Result: " + result);
     }
-  
+
+    // Function to calculate power
+    public static double power(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n % 2 == 0) {
+            // If n is even, x^n = (x^(n/2))^2
+            double temp = power(x, n / 2);
+            return temp * temp;
+        } else {
+            // If n is odd, x^n = x * (x^(n-1))
+            return x * power(x, n - 1);
+        }
+    }
+
 }
     
